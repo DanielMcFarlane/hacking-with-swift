bea8692cc3ec2f5c398c3da4e17edac49c73e847
Daniel <130492222+DanielMcFarlane@users.noreply.github.com>
2025-04-06 17:32:58 +0100

    Day 2 - Concatenation and interpolation

---
+++ Hacking with Swift.playground/Pages/Day 2 - Concatenation and interpolation.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+// Concatenation
+let firstPart = "Hello, "
+let secondPart = "Word!"
+
+print(firstPart + secondPart)
+
+
+// Interpolation
+let name = "Daniel"
+let age = 29
+print("Hello, my name is \(name) and I am \(age) years old") // \() is like f strings in python
