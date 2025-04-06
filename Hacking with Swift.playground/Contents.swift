8ed4b6a0f84bf0e9e5e94d0cf890c0c652395474
Daniel <130492222+DanielMcFarlane@users.noreply.github.com>
2025-04-06 16:09:08 +0100

    Day 1 - Variables, constants and strings

--- Hacking with Swift.playground/Contents.swift
+++ Hacking with Swift.playground/Contents.swift
@@ -1,3 +1,44 @@
 import Cocoa
 
+
+// Variables use var
 var greeting = "Hello, playground"
+print(greeting)
+
+greeting = "Hello, world!"
+print(greeting)
+
+
+// Constants use let
+let name = "Daniel"
+print("Hello, \(name)!")
+
+
+// Inner quotes need a \ before them
+let quote = "The future belongs to those who \"believe\" in the possibilities of tomorrow."
+print(quote)
+
+
+// Multi line strings use """
+let anotherQuote = """
+The future belongs to those
+who believe in the possibilities 
+of tomorrow.
+"""
+print(anotherQuote)
+
+
+// Count the number of characters in a string
+print(greeting.count)
+
+
+// Convert to uppercase
+print(quote.uppercased())
+
+
+// Find if a string starts with selected words
+print(quote.hasPrefix("The"))
+
+
+// Find if a string ends with selected words
+print(quote.hasSuffix("tomorrow."))
