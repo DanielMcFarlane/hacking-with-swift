fd32c05d0606dbcba613ccc06d6406df42419b99
Daniel <130492222+DanielMcFarlane@users.noreply.github.com>
2025-04-08 13:56:21 +0100

    Day 7 - Functions

--- Hacking with Swift.playground/Pages/Day 6 - FizzBuzz.xcplaygroundpage/Contents.swift
+++ Hacking with Swift.playground/Pages/Day 6 - FizzBuzz.xcplaygroundpage/Contents.swift
@@ -4,5 +4,5 @@ for number in 1 ... 100 {
     var output = ""
     if number % 3 == 0 { output += "Fizz" }
     if number % 5 == 0 { output += "Buzz" }
-    print(output.isEmpty ? number : output)
+    print((output.isEmpty) ? number : output)
 }
