8ce3704607748ce4c9d6bcfd6704414f29822c2d
Daniel <130492222+DanielMcFarlane@users.noreply.github.com>
2025-04-10 14:46:17 +0100

    Day 10 - Custom intialisers

---
+++ Hacking with Swift.playground/Pages/Day 10 - Custom intialisers.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+// init can be used within the struct to create a new instance
+struct Player {
+    let name: String
+    let number: Int
+
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+print(player.number)
