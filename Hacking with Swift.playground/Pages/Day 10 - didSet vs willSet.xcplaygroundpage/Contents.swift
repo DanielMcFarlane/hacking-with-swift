785646218543ffec0f392a211fde930023f943cf
Daniel <130492222+DanielMcFarlane@users.noreply.github.com>
2025-04-10 14:39:02 +0100

    Day 10 - didSet vs willSet

---
+++ Hacking with Swift.playground/Pages/Day 10 - didSet vs willSet.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+import Cocoa
+
+// didSet runs after the score is changed, showing the old and new values
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+
+// willSet runs before the score is changed, showing the new value
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
