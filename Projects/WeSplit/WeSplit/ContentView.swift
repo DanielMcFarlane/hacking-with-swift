811160d1b0907ee6bc87fa7e2b7c9ee64b7bf935
Daniel <130492222+DanielMcFarlane@users.noreply.github.com>
2025-04-14 15:51:34 +0100

    WeSplit Part 10 - Calculating the total per person

--- Projects/WeSplit/WeSplit/ContentView.swift
+++ Projects/WeSplit/WeSplit/ContentView.swift
@@ -11,37 +11,50 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
-    
+
     let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var currencyCode: String {
+        Locale.current.currency?.identifier ?? "USD"
+    }
+
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationStack {
             Form {
-                Section ("Input") {
-                    Section {
-                        TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                            .keyboardType(.decimalPad)
-                        
-                        Picker("Number of people", selection: $numberOfPeople) {
-                            ForEach(2 ..< 100) {
-                                Text("\($0) people")
-                            }
+                Section("Input") {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: currencyCode))
+                        .keyboardType(.decimalPad)
+
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
                         }
-                        .pickerStyle(.navigationLink) // Creates an overlay vs drop down
                     }
+                    .pickerStyle(.navigationLink) // Creates an overlay instead of drop-down
                 }
-                
+
                 Section("How much tip do you want to leave?") {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(tipPercentages, id: \.self) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)  // Add a segmented picker isntead of drop down
+                    .pickerStyle(.segmented) // Adds a segmented picker instead of drop-down
                 }
-                
-                Section ("Output"){
-                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "GBP"))
+
+                Section("Output") {
+                    Text("Each person will pay \(totalPerPerson, format: .currency(code: currencyCode))")
                 }
             }
             .navigationTitle("WeSplit")
