50aa00ec77652660b177e5bd501cdbb2dbbcd9cf
Daniel <130492222+DanielMcFarlane@users.noreply.github.com>
2025-04-15 12:52:52 +0100

    Guess the Flag Part 3 - Gradients

--- Projects/Guess The Flag/Guess The Flag/ContentView.swift
+++ Projects/Guess The Flag/Guess The Flag/ContentView.swift
@@ -9,21 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                Color.red
-                Color.blue
-            }
-
-            Text("Your content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial) // Might be good for graded unit
-        }
-        .ignoresSafeArea()
+        Text("Your content")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .foregroundStyle(.white)
+            .background(.indigo
+                .gradient)
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+//LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
+//
+//LinearGradient(stops: [
+//    Gradient.Stop(color: .white, location: 0.45),
+//    Gradient.Stop(color: .black, location: 0.55),
+//], startPoint: .top, endPoint: .bottom)
+//
+//LinearGradient(stops: [
+//    .init(color: .white, location: 0.45),
+//    .init(color: .black, location: 0.55),
+//], startPoint: .top, endPoint: .bottom)
+//
+//RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
+//
+//AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
