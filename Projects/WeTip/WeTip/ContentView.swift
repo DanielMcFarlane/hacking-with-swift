45a4fe13f39ae3cd58a5db159f25d0d26003f2cb
Daniel <130492222+DanielMcFarlane@users.noreply.github.com>
2025-04-15 15:53:48 +0100

    WeTip

--- Projects/WeTip/WeTip/ContentView.swift
+++ Projects/WeTip/WeTip/ContentView.swift
@@ -1,15 +1,15 @@
 //
 //  ContentView.swift
-//  WeSplit
+//  WeTip
 //
-//  Created by College on 14/04/2025.
+//  Created by Daniel McFarlane on 14/04/2025.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = 1
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
     
@@ -20,7 +20,7 @@ struct ContentView: View {
     }
 
     var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+        let peopleCount = Double(numberOfPeople)
         let tipSelection = Double(tipPercentage)
 
         let tipValue = checkAmount / 100 * tipSelection
@@ -44,13 +44,13 @@ struct ContentView: View {
 
                 Section("Number of People") {
                     Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2 ..< 100) {
-                            Text("\($0) people")
+                        ForEach(1 ..< 100, id: \.self) {
+                            Text($0 == 1 ? "1 person" : "\($0) people")
                         }
                     }
                     .pickerStyle(.navigationLink)
                 }
-
+                
                 Section("How much tip do you want to leave?") {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(tipPercentages, id: \.self) {
@@ -61,10 +61,14 @@ struct ContentView: View {
                 }
 
                 Section("Output") {
-                    Text("Each person will pay \(totalPerPerson, format: .currency(code: currencyCode))")
+                    if numberOfPeople == 1 {
+                        Text("You will pay \(totalPerPerson, format: .currency(code: currencyCode))")
+                    } else {
+                        Text("Each person will pay \(totalPerPerson, format: .currency(code: currencyCode))")
+                    }
                 }
             }
-            .navigationTitle("WeSplit")
+            .navigationTitle("WeTip")
             .toolbar {
                 if amountIsFocused {
                     Button("Done") {
