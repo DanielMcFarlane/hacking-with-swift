b3c8fb474f2f31f1a3c7f254702653cba39a0afa
Daniel <130492222+DanielMcFarlane@users.noreply.github.com>
2025-04-17 15:58:47 +0100

    BetterRest Part 3 - Working with dates

--- Projects/BetterRest/BetterRest/ContentView.swift
+++ Projects/BetterRest/BetterRest/ContentView.swift
@@ -8,26 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
-    
     var body: some View {
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+        Text(Date.now, format: .dateTime.hour().minute())
+        Text(Date.now, format: .dateTime.day().month().year())
+        Text(Date.now.formatted(date: .long, time: .shortened))
     }
     
     func exampleDates() {
-        // create a second Date instance set to one day in seconds from now
-        let tomorrow = Date.now.addingTimeInterval(86400)
-
-        // create a range from those two
-        let range = Date.now...tomorrow
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? .now
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
 #Preview {
     ContentView()
 }
-
-
-// Use this if you want to hide text so that screen readers still pick it up
-//DatePicker("Please enter a date", selection: $wakeUp)
-//    .labelsHidden()
